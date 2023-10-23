9cda27f59ccee2cfb23bbdfd93c63794516dad90
Oryn <G00406032@atu.ie>
2023-10-23 16:49:21 +0100

    Feature 1 added

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,9 +1,15 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println();
-        Person student1 = new Person("Oryn","Quinlan",21);
+        Person person1 = new Person("Oryn","Quinlan",35);
+        person1.displayInfo();
+
+        Person person2 = new Person();
+        person2.getUserInput();
+        person2.displayInfo();
     }
 }
