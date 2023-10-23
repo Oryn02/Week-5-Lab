6cd3a64e7456ceb1773716cf794e678fc1a58bac
Oryn <G00406032@atu.ie>
2023-10-23 16:30:17 +0100

    new project created

---
+++ src/main/java/ie/atu/Main.java
@@ -0,0 +1,9 @@
+package ie.atu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println();
+        Person student1 = new Person("Oryn","Quinlan",21);
+    }
+}
